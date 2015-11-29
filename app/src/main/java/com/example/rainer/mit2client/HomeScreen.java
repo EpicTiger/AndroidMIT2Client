f98dc653bbc1b3c910187b7c7e01c12ffdf958a7
Wiel Kruiwagen <lexus8801@hotmail.com>
2015-11-29 13:18:15 +0100

    Added user Entity

--- app/src/main/java/com/example/rainer/mit2client/HomeScreen.java
+++ app/src/main/java/com/example/rainer/mit2client/HomeScreen.java
@@ -2,6 +2,9 @@ package com.example.rainer.mit2client;
 
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+
+import Entities.User;
 
 public class HomeScreen extends BaseView {
 
@@ -14,10 +17,29 @@ public class HomeScreen extends BaseView {
 
         initializeToolbar();
         initializeNavigationDrawer(toolbar);
+        setupDebugUser();
+        initializeData();
     }
 
     private void initializeToolbar(){
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    private void initializeData() {
+
+        TextView fullname = (TextView) findViewById(R.id.homescreen_textview_fullname);
+
+        fullname.setText(User.Fullname);
+    }
+
+    private void setupDebugUser(){
+        User.Username = "Debuguser";
+        User.Fullname = "Debug User";
+        User.Gender = "Male";
+        User.PoliticalPreference = "N/A";
+        User.Nationality = "Dutch";
+        User.DateOfBirth = "1 - 1 - 1970";
+        User.Town = "Amsterdam";
     }
 }
