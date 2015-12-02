2269668c1cc3304e7315c1937a1c80e1e630ec58
Wiel Kruiwagen <lexus8801@hotmail.com>
2015-12-02 16:32:29 +0100

    Screen data fixed

--- app/src/main/java/com/example/rainer/mit2client/HomeFragment.java
+++ app/src/main/java/com/example/rainer/mit2client/HomeFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import Entities.User;
 
 public class HomeFragment extends Fragment {
 
@@ -13,6 +16,15 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.content_home_screen, container, false);
+        View view = inflater.inflate(R.layout.content_home_screen, container, false);
+        initializeData(view);
+        return view;
+    }
+
+    private void initializeData(View view) {
+
+        TextView fullname = (TextView) view.findViewById(R.id.homescreen_textview_fullname);
+
+        fullname.setText(User.Fullname);
     }
 }
--- app/src/main/java/com/example/rainer/mit2client/ProfileFragment.java
+++ app/src/main/java/com/example/rainer/mit2client/ProfileFragment.java
@@ -6,6 +6,9 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import Entities.User;
 
 public class ProfileFragment extends Fragment {
 
@@ -13,23 +16,25 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
-        return inflater.inflate(R.layout.content_profile_page, container, false);
+        View view = inflater.inflate(R.layout.content_profile_page, container, false);
+        initializeData(view);
+        return view;
     }
 
-//    private void initializeData() {
-//
-//        TextView fullname = (TextView) findViewById(R.id.profilescreen_textview_fullname);
-//        TextView gender = (TextView) findViewById(R.id.profilescreen_textview_gender);
-//        TextView nationality = (TextView) findViewById(R.id.profilescreen_textview_nationality);
-//        TextView dateofbirth = (TextView) findViewById(R.id.profilescreen_textview_dateofbirth);
-//        TextView politicalpreference = (TextView) findViewById(R.id.profilescreen_textview_politicalpreference);
-//        TextView town = (TextView) findViewById(R.id.profilescreen_textview_town);
-//
-//        fullname.setText(User.Fullname);
-//        gender.setText(User.Gender);
-//        nationality.setText(User.Nationality);
-//        dateofbirth.setText(User.DateOfBirth);
-//        politicalpreference.setText(User.PoliticalPreference);
-//        town.setText(User.Town);
-//    }
+    private void initializeData(View view) {
+
+        TextView fullname = (TextView) view.findViewById(R.id.profilescreen_textview_fullname);
+        TextView gender = (TextView) view.findViewById(R.id.profilescreen_textview_gender);
+        TextView nationality = (TextView) view.findViewById(R.id.profilescreen_textview_nationality);
+        TextView dateofbirth = (TextView) view.findViewById(R.id.profilescreen_textview_dateofbirth);
+        TextView politicalpreference = (TextView) view.findViewById(R.id.profilescreen_textview_politicalpreference);
+        TextView town = (TextView) view.findViewById(R.id.profilescreen_textview_town);
+
+        fullname.setText(User.Fullname);
+        gender.setText(User.Gender);
+        nationality.setText(User.Nationality);
+        dateofbirth.setText(User.DateOfBirth);
+        politicalpreference.setText(User.PoliticalPreference);
+        town.setText(User.Town);
+    }
 }
