d8a3f0b7ea91e188552a708e637f26d837d44337
EpicTiger <rainerschmitt123@gmail.com>
2016-01-06 14:11:53 +0100

    Fixed Thift

--- app/src/main/java/com/example/rainer/mit2client/LoginActivity.java
+++ app/src/main/java/com/example/rainer/mit2client/LoginActivity.java
@@ -1,24 +1,27 @@
 package com.example.rainer.mit2client;
 
 import android.app.FragmentManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import org.apache.thrift.TException;
 import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 import org.apache.thrift.transport.TSocket;
 import org.apache.thrift.transport.TTransport;
 
-import Generated.AuthenticationClientService;
-import Generated.LoginData;
-import Generated.LoginResult;
+import Politics247Generated.AuthenticationClientService;
+import Politics247Generated.LoginData;
+import Politics247Generated.LoginResult;
 
-public class LoginActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener{
+
+public class LoginActivity extends AppCompatActivity implements FragmentManager.OnBackStackChangedListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         ExexuteThrift();
@@ -38,40 +41,31 @@ public class LoginActivity extends AppCompatActivity implements FragmentManager.
 
     private void ExexuteThrift()
     {
-        try {
-            TTransport transport;
+        LoginData loginData = new LoginData();
+        loginData.setEmail("dwahkdwa");
+        loginData.setPassword("dawdwa");
 
-            transport = new TSocket("10.77.140.64", 9090);
-            transport.open();
-
-            TProtocol protocol = new TBinaryProtocol(transport);
-            AuthenticationClientService.Client client = new AuthenticationClientService.Client(protocol);
-
-            LoginData loginData = new LoginData();
-            loginData.setEmail("dwahkdwa");
-            loginData.setPassword("dawdwa");
-
-            LoginResult result = client.Login(loginData);
-
-            transport.close();
-        } catch (TException x) {
-            x.printStackTrace();
-        }
+    new LoginThriftClass().execute(loginData);
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         int count = getFragmentManager().getBackStackEntryCount();
 
-        if (count == 0) {
+        if (count == 0)
+        {
             super.onBackPressed();
-        } else {
+        } else
+        {
             getFragmentManager().popBackStack();
         }
     }
 
     @Override
-    public void onBackStackChanged() {
+    public void onBackStackChanged()
+    {
         Log.v("FragmentManager", "Changefragment");
     }
 }
+
--- app/src/main/java/com/example/rainer/mit2client/LoginFragment.java
+++ app/src/main/java/com/example/rainer/mit2client/LoginFragment.java
@@ -8,9 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import Generated.AuthenticationClientService;
-import Generated.LoginData;
-
 public class LoginFragment extends Fragment
 {
 
